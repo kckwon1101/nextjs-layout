0e7fd719eead1bf41882e355ddbcb1189ea8f3fe
권기창 <kichang.kwon@aist.co.kr>
2023-01-29 01:26:45 +0900

    add react-icons/tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  // darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        // LG: '#A50034'
+        LG: '#C30340'
+        // LG: '#D9004D'
+      }
+    },
+  },
+  plugins: [],
+}
